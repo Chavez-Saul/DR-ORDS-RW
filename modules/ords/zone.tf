7bb3f6f0e237432ec1a70a74b683e8565a1e529d
saul <saul.chavez@oracle.com>
2020-09-02 16:00:43 -0500

    Removed app server, lb balancer, and edited the security list.

---
+++ modules/ords/zone.tf
@@ -0,0 +1,7 @@
+resource "oci_dns_zone" "ORDS-Zone" {
+  count = 1 - var.Secure_FQDN_access # if Secure_FQDN_access=0 then Zone resource is configured
+
+  compartment_id = var.compartment_ocid
+  name           = var.ZoneName
+  zone_type      = "PRIMARY"
+}
